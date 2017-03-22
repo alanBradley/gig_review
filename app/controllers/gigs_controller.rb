177febed338d370716ffe56901b5ae0f8075fdf0
Bradley <alan.bradley@live.ie>
2017-03-22 14:19:25 +0000

    Add index & show actions

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,6 +1,11 @@
 class GigsController < ApplicationController
+  before_action :find_gig, only: [:show, :edit, :update, :destroy]
   
   def index
+    @gigs = Gig.all.order("created_at DESC")
+  end
+  
+  def show
   end
   
   def new
@@ -10,12 +15,22 @@ class GigsController < ApplicationController
   def create
     # @gig = Gig.new(params.require(:gig).permit(:title, :description, :promoter))
     @gig = Gig.new(gig_params)
+    
+    if @gig.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
   
   private
   
   def gig_params
     params.require(:gig).permit(:title, :description, :promoter)
+  end
+  
+  def find_gig
+    @gig = Gig.find(params[:id])
   end
   
   
